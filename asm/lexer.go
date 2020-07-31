193a3041589bb338055cacfd77bdac2d47617b9b
Jos Visser <josv@fb.com>
2020-07-30 22:36:53 -0400

    Lexing of numbers

--- asm/lexer.go
+++ asm/lexer.go
@@ -1,6 +1,11 @@
 package asm
 
-import "unicode"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
+)
 
 type token int
 
@@ -8,13 +13,15 @@ const (
 	tokEOF token = iota
 	tokNewLine
 	tokIdentifier
-	tokUnknown
+	tokNumber
+	tokChar
+	tokError
 )
 
 // getWord returns the next word from the stream. firstRune is the first
 // (starter) rune for the word, which has already been consumed from
 // the string.
-func (s * source) getWord(firstRune rune) string {
+func (s *source) getWord(firstRune rune) string {
 	word := make([]rune, 1, 64)
 	word[0] = firstRune
 	for {
@@ -27,17 +34,47 @@ func (s * source) getWord(firstRune rune) string {
 	}
 }
 
+// getHexString returns the next hex/bin/decimal digit string from the stream.
+func (s *source) getString(firstRune rune, allowed string) string {
+	str := make([]rune, 0, 32)
+	if firstRune != 0 {
+		str = append(str, firstRune)
+	}
+	for {
+		r, eof := s.peekRune()
+		r = unicode.ToLower(r)
+		if eof || strings.IndexRune(allowed, r) == -1 {
+			return string(str)
+		}
+		str = append(str, r)
+		s.consumeRune()
+	}
+}
+
+// getNumber returns a number from the stream.
+func (s *source) getNumber(firstRune rune, base int, allowed string) (t token, text string, val int64, eof bool) {
+	str := s.getString(firstRune, allowed)
+	if str == "" {
+		return tokError, "illegal number <empty string>", 0, false
+	}
+	i, err := strconv.ParseInt(str, base, 64)
+	if err != nil {
+		return tokError, fmt.Sprintf("number too large (base %d): %s", base, str), 0, false
+	}
+	return tokNumber, fmt.Sprintf("%s", str), i, false
+}
+
 // getToken returns the next token in the stream.
-func (s *source) getToken() (t token, text string, eof bool) {
+func (s *source) getToken() (t token, text string, val int64, eof bool) {
 	var r rune
 	for {
 		var eof bool
 		r, eof = s.consumeRune()
 		if eof {
-			return tokEOF, "", eof
+			return tokEOF, "", 0, eof
 		}
 		if r == '\n' {
-			return tokNewLine, "\n", false
+			return tokNewLine, "\n", 0, false
 		}
 		if !unicode.IsSpace(r) {
 			break
@@ -46,12 +83,32 @@ func (s *source) getToken() (t token, text string, eof bool) {
 	if r == ';' {
 		// Ignores comments.
 		s.skipToEOLN()
-		return tokNewLine, "\n", false
+		return tokNewLine, "\n", 0, false
 	}
 	r = unicode.ToLower(r)
 	if unicode.IsLetter(r) || r == '_' {
 		// Identifier or keyword.
-		return tokIdentifier, s.getWord(r), false
+		return tokIdentifier, s.getWord(r), 0, false
 	}
-	return tokUnknown, string(r), false
+	if r == '0' {
+		// A number.
+		r, eof = s.peekRune()
+		if r == '\n' || eof {
+			// A number just before the newline.
+			return tokNumber, "0", 0, false
+		}
+		if r == 'x' {
+			s.consumeRune()
+			return s.getNumber(0, 16, "0123456789abcdef")
+		}
+		if r == 'b' {
+			s.consumeRune()
+			return s.getNumber(0, 2, "01")
+		}
+		return s.getNumber('0', 8, "01234567")
+	}
+	if unicode.IsDigit(r) {
+		return s.getNumber(r, 10, "0123456789")
+	}
+	return tokChar, string(r), 0, false
 }
--- asm/lexer_test.go
+++ asm/lexer_test.go
@@ -3,11 +3,11 @@ package asm
 import "testing"
 
 func TestTokenizeIdentifiers(t *testing.T) {
-	src := newSourceFromString("  aAp _NOOT Mies123 _wim12_foo ; comment")
+	src := newSourceFromString("  aAp\t_NOOT\rMies123 _wim12_foo ; comment")
 	want := []string{"aap", "_noot", "mies123", "_wim12_foo"}
 	got := make([]string, 0, 4)
 	for {
-		tok, s, eof := src.getToken()
+		tok, s, _, eof := src.getToken()
 		if eof {
 			t.Errorf("eof; got:true, want:false")
 			break
@@ -20,16 +20,65 @@ func TestTokenizeIdentifiers(t *testing.T) {
 		}
 		got = append(got, s)
 	}
-	tok, _, _ := src.getToken()
+	tok, _, _, _ := src.getToken()
 	if tok != tokEOF {
 		t.Errorf("getToken(); got:%d, want:%d", tok, tokEOF)
 	}
 	if len(got) != len(want) {
 		t.Errorf("len(got); got:%d, want:%d", len(got), len(want))
 	}
-	for i:=0; i < len(got); i++ {
+	for i := 0; i < len(got); i++ {
 		if got[i] != want[i] {
 			t.Errorf("got[%d]; got:%s, want:%s", i, got[i], want[i])
 		}
+	}
+}
+
+func TestTokenizeNumbers(t *testing.T) {
+	src := newSourceFromString("0 010 0b101 0x4Af 42")
+	got := make([]int64, 0, 5)
+	want := []int64{0, 8, 5, 0x4af, 42}
+	for {
+		tok, _, i, eof := src.getToken()
+		if tok == tokNewLine {
+			break
+		}
+		if eof {
+			t.Fatalf("eof; got:true, want:false")
+		}
+		if tok != tokNumber {
+			t.Fatalf("tok; got:%d, want:%d", tok, tokNumber)
+		}
+		got = append(got, i)
+	}
+	if len(got) != len(want) {
+		t.Errorf("len(got); got:%d, want:%d", len(got), len(want))
+	}
+	for i := 0; i < len(got); i++ {
+		if got[i] != want[i] {
+			t.Errorf("got[%d]; got:%d, want:%d", i, got[i], want[i])
+		}
+	}
+}
+
+func TestTokenizeBadHexNumber1(t *testing.T) {
+	src := newSourceFromString("0x")
+	tok, _, _, eof := src.getToken()
+	if eof {
+		t.Fatalf("eof; got:true, want:false")
+	}
+	if tok != tokError {
+		t.Fatalf("tok; got:%d, want:%d", tok, tokError)
+	}
+}
+
+func TestTokenizeBadHexNumber2(t *testing.T) {
+	src := newSourceFromString("0xg")
+	tok, _, _, eof := src.getToken()
+	if eof {
+		t.Fatalf("eof; got:true, want:false")
+	}
+	if tok != tokError {
+		t.Fatalf("tok; got:%d, want:%d", tok, tokError)
 	}
 }
