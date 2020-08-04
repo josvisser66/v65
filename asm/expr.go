24650a13e096e42394f626237ce0f0ffe512b427
Jos Visser <josv@fb.com>
2020-08-04 07:50:11 -0400

    Expressions kinda done

--- asm/expr.go
+++ asm/expr.go
@@ -11,17 +11,25 @@ func (s *source) expr(seg *segment) (val *exprValue, next token) {
 
 	// If this token is an identifier, there is a change that we have
 	// an expression that requires relocation. Such an expression starts
-	// with an external symbol and then a positive or negative offset.
+	// with an external symbol and then a positive or negative constant
+	// offset.
 	if id, ok := tok.(*tokIdentifier); ok {
 		var sym symbol
 		if sym, ok = seg.symbols[id.id]; !ok {
-			seg.error(s, "unknown label: %s", id.id)
-			v, next := s.level1(seg, s.getToken())
+			// The error will be generated down there somewhere.
+			v, next := s.level1(seg, tok)
 			return &exprValue{nil, v}, next
 		}
 		if externSym, ok := sym.(*externSymbol); ok {
-			// This is an external symbol.
-			v, next := s.level1(seg, s.getToken())
+			// This is an external symbol. The rest of the expression
+			// can be + or - something else.
+			next := s.getToken()
+			var v int64
+			if _, ok := next.(*tokPlus); ok {
+				v, next = s.level1(seg, nil)
+			} else if _, ok := next.(*tokMinus); ok {
+				v, next = s.level1(seg, nil)
+			}
 			return &exprValue{externSym, v}, next
 		}
 		// The identifier is a label with a known value. Fallthrough.
@@ -33,20 +41,63 @@ func (s *source) expr(seg *segment) (val *exprValue, next token) {
 
 func (s *source) level1(seg *segment, nextToken token) (val int64, next token) {
 	val, next = s.level2(seg, nextToken)
-
-	return val, next
+	var v int64
+	for {
+		if next == nil {
+			next = s.getToken()
+		}
+		if _, ok := next.(*tokOr); ok {
+			v, next = s.level2(seg, nil)
+			val = val | v
+		} else if _, ok := next.(*tokAnd); ok {
+			v, next = s.level2(seg, nil)
+			val = val & v
+		} else {
+			return val, next
+		}
+	}
 }
 
 func (s *source) level2(seg *segment, nextToken token) (val int64, next token) {
 	val, next = s.level3(seg, nextToken)
-
-	return val, next
+	var v int64
+	for {
+		if next == nil {
+			next = s.getToken()
+		}
+		if _, ok := next.(*tokPlus); ok {
+			v, next = s.level3(seg, nil)
+			val = val + v
+		} else if _, ok := next.(*tokMinus); ok {
+			v, next = s.level3(seg, nil)
+				val = val - v
+		} else {
+			return val, next
+		}
+	}
 }
 
 func (s *source) level3(seg *segment, nextToken token) (val int64, next token) {
 	val, next = s.level4(seg, nextToken)
-
-	return val, next
+	var v int64
+	for {
+		if next == nil {
+			next = s.getToken()
+		}
+		if _, ok := next.(*tokMultiply); ok {
+			v, next = s.level4(seg, nil)
+			val = val * v
+		} else if _, ok := next.(*tokDivide); ok {
+			v, next = s.level4(seg, nil)
+			if v == 0 {
+				seg.error(s, "division by zero")
+			} else {
+				val = val / v
+			}
+		} else {
+			return val, next
+		}
+	}
 }
 
 func (s *source) level4(seg *segment, nextToken token) (val int64, next token) {
@@ -61,13 +112,36 @@ func (s *source) level4(seg *segment, nextToken token) (val int64, next token) {
 		return num.n, s.getToken()
 	}
 	if _, ok := nextToken.(*tokLeftParen); ok {
-		val, next := s.level1(seg, nil)
+		v, next := s.level1(seg, nil)
 		if _, ok := next.(*tokRightParen); !ok {
 			seg.error(s, "expected ')', not '%T'", next)
 			s.skipToEOLN()
 			return 0, nil
 		}
-		return val , nil
+		return v , nil
+	}
+	if id, ok := nextToken.(*tokIdentifier); ok {
+		// Label, must be locally defined.
+		sym, ok := seg.symbols[id.id]
+		if !ok {
+			seg.error(s, "unknown label: %s", id.id)
+			return 0, nil
+		}
+		if localSym, ok := sym.(*localSymbol); ok {
+			return localSym.value, nil
+		}
+		seg.error(s, "illegal use in expression of external label: %s", id.id)
+		return 0, nil
+	}
+	if _, ok := nextToken.(*tokPlus); ok {
+		// Unary plus operator.
+		v, next := s.level4(seg, nil)
+		return v, next
+	}
+	if _, ok := nextToken.(*tokMinus); ok {
+		// Unary minus operator.
+		v, next := s.level4(seg, nil)
+		return -v, next
 	}
 	seg.error(s, "invalid expression; unexpected token: '%T'", nextToken)
 	s.skipRestOfLine()
--- asm/expr_test.go
+++ asm/expr_test.go
@@ -4,24 +4,71 @@ import "testing"
 
 func TestExpressionEval(t *testing.T) {
 	seg := newSegment()
+	seg.symbols["fu"] = &externSymbol{"fu"}
+	seg.symbols["bar"] = &localSymbol{"bar", 7, 16, false}
 	for _, tc := range []struct {
-		str      string
-		wantNum  int64
+		str        string
+		wantNum    int64
+		wantSym bool
 		wantErrors int
-		wantNext func(t token) bool
+		wantNext   func(t token) bool
 	}{
-		{"42", 42, 0,func(t token) bool {
+		{"42", 42, false,0, func(t token) bool {
 			_, ok := t.(*tokNewLine)
 			return ok
 		}},
-		{"42,", 42,0, func(t token) bool {
+		{"-40*2/4", -20, false,0, func(t token) bool {
+			_, ok := t.(*tokNewLine)
+			return ok
+		}},
+		{"-40*2/4+10*7", 50, false,0, func(t token) bool {
+			_, ok := t.(*tokNewLine)
+			return ok
+		}},
+		{"65535 & 255", 255, false,0, func(t token) bool {
+			_, ok := t.(*tokNewLine)
+			return ok
+		}},
+		{"2 | 1", 3, false,0, func(t token) bool {
+			_, ok := t.(*tokNewLine)
+			return ok
+		}},
+		{"+42", 42, false,0, func(t token) bool {
+			_, ok := t.(*tokNewLine)
+			return ok
+		}},
+		{"-42", -42, false,0, func(t token) bool {
+			_, ok := t.(*tokNewLine)
+			return ok
+		}},
+		{"--42", 42, false,0, func(t token) bool {
+			_, ok := t.(*tokNewLine)
+			return ok
+		}},
+		{"fu", 0, true, 0, func(t token) bool {
+			_, ok := t.(*tokNewLine)
+			return ok
+		}},
+		{"bar", 7, false, 0, func(t token) bool {
+			_, ok := t.(*tokNewLine)
+			return ok
+		}},
+		{"baz", 0, false, 1, func(t token) bool {
+			_, ok := t.(*tokNewLine)
+			return ok
+		}},
+		{"42", 42, false, 0, func(t token) bool {
+			_, ok := t.(*tokNewLine)
+			return ok
+		}},
+		{"42,", 42, false, 0, func(t token) bool {
 			_, ok := t.(*tokComma)
 			return ok
-		}},{"(42),", 42,0, func(t token) bool {
+		}}, {"(42),", 42, false, 0, func(t token) bool {
 			_, ok := t.(*tokComma)
 			return ok
 		}},
-		{"(42", 0,1, func(t token) bool {
+		{"(42", 0, false, 1, func(t token) bool {
 			_, ok := t.(*tokNewLine)
 			return ok
 		}},
@@ -33,14 +80,17 @@ func TestExpressionEval(t *testing.T) {
 		if next == nil {
 			next = src.getToken()
 		}
+		if (val.sym == nil && tc.wantSym) || (val.sym!= nil && !tc.wantSym) {
+			t.Errorf("val.sym: got:%v, want-nil:%v", val.sym, tc.wantSym)
+		}
 		if val.val != tc.wantNum {
 			t.Errorf("expr(%s); got:%d, want:%d", tc.str, val.val, tc.wantNum)
 		}
 		if !tc.wantNext(next) {
 			t.Error("tc.wantNext(t); got:false, want:true")
 		}
-		if saveErrors + tc.wantErrors != seg.errors {
-			t.Errorf("seg.errors; got:%d, want:%d", seg.errors, saveErrors + tc.wantErrors)
+		if saveErrors+tc.wantErrors != seg.errors {
+			t.Errorf("seg.errors; got:%d, want:%d", seg.errors, saveErrors+tc.wantErrors)
 		}
 	}
 }
