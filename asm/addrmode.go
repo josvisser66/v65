f080635e5bb3846a07a1093ae499f91fb5af0189
Jos Visser <josv@fb.com>
2020-08-15 14:23:31 -0400

    Fixed addressing modes (indexes and error handling)

--- asm/addrmode.go
+++ asm/addrmode.go
@@ -1,28 +1,33 @@
 package asm
 
+import "errors"
+
 // Here is a good reference of 6502 addressing modes:
 // http://www.obelisk.me.uk/6502/addressing.html
 
 const (
-	errorAddrMode   = iota
-	implicit        // <epsilon>
-	accumulator     // A
-	immediate       // #<expression>
-	zeroPage        // <expression>
-	zeroPageX       // <expression>, X
-	zeroPageY       // <expression>, Y
-	relative        // for branches
-	absolute        // <expression>
-	absoluteX       // <expression>, X
-	absoluteY       // <expression>, Y
-	indirect        // (<expression>)
-	indexedIndirect // (<expression>, X)
-	indirectIndexed // (<expression>), Y
+	// Note: Do not change the values in this table, because they match
+	// indices in slices in the opcodes maps.
+	implicit        = 0  // <epsilon>
+	accumulator     = 1  // A
+	immediate       = 4  // #<expression>
+	zeroPage        = 3  // <expression>
+	zeroPageX       = 9  // <expression>, X
+	zeroPageY       = 10 // <expression>, Y
+	relative        = 11 // for branches
+	absolute        = 2  // <expression>
+	absoluteX       = 5  // <expression>, X
+	absoluteY       = 6  // <expression>, Y
+	indirect        = 12 // (<expression>)
+	indexedIndirect = 7  // (<expression>, X)
+	indirectIndexed = 8  // (<expression>), Y
 )
+
+var errorAddressingMode = errors.New("illegal addressing mode")
 
 // parseIndirect parses addressing modes that start with an lparen. When
 // the function is parsed the tokLparen needs to have already been parsed.
-func (ctx *context) parseIndirect() (int, *exprValue) {
+func (ctx *context) parseIndirect() (int, *exprValue, error) {
 	val := ctx.expr()
 	next := ctx.lexer.getToken()
 	switch next.(type) {
@@ -31,73 +36,73 @@ func (ctx *context) parseIndirect() (int, *exprValue) {
 		next = ctx.lexer.getToken()
 		if _, ok := next.(*tokRegisterX); !ok {
 			ctx.error("expected X, not: '%T'", next)
-			return errorAddrMode, nil
+			return -1, nil, errorAddressingMode
 		}
 		next = ctx.lexer.getToken()
 		if _, ok := next.(*tokRightParen); !ok {
 			ctx.error("expected ), not: '%T'", next)
-			return errorAddrMode, nil
+			return -1, nil, errorAddressingMode
 		}
-		return indexedIndirect, val
+		return indexedIndirect, val, nil
 	case *tokRightParen:
 		// Can be (expr) or (expr), Y
 		next = ctx.lexer.getToken()
 		if _, ok := next.(*tokNewLine); ok {
 			// Is (expr) => Indirect.
-			return indirect, val
+			return indirect, val, nil
 		}
 		// Must be (expr), Y => Indirect indexed.
 		if _, ok := next.(*tokComma); !ok {
 			ctx.error("expected comma, not: '%T'", next)
-			return errorAddrMode, nil
+			return -1, nil, errorAddressingMode
 		}
 		next = ctx.lexer.getToken()
 		if _, ok := next.(*tokRegisterY); !ok {
 			ctx.error("expected Y, not: '%T'", next)
-			return errorAddrMode, nil
+			return -1, nil, errorAddressingMode
 		}
-		return indirectIndexed, val
+		return indirectIndexed, val, nil
 	}
 	ctx.error("unexpected token: '%T'", next)
-	return errorAddrMode, nil
+	return -1, nil, errorAddressingMode
 }
 
 // parseAbsolute parses addressing modes that start with an expression.
 // Either expr or expr,X or expr,Y.
-func (ctx *context) parseAbsolute() (int, *exprValue) {
+func (ctx *context) parseAbsolute() (int, *exprValue, error) {
 	val := ctx.expr()
 	next := ctx.lexer.getToken()
 	switch next.(type) {
 	case *tokNewLine:
 		// If it was meant to be relative or zero page this gets resolved
 		// later.
-		return absolute, val
+		return absolute, val, nil
 	case *tokComma:
 		next = ctx.lexer.getToken()
 		switch next.(type) {
 		case *tokRegisterX:
-			return absoluteX, val
+			return absoluteX, val, nil
 		case *tokRegisterY:
-			return absoluteY, val
+			return absoluteY, val, nil
 		}
 	}
 	ctx.error("unexpected token: '%T'", next)
-	return errorAddrMode, nil
+	return -1, nil, errorAddressingMode
 }
 
 // parseAddressingMode parses an addressing mode. It does not give
 // any errors about sizes or illegal use of external symbols. This
 // will be done during stuffing the bytes into the code segment.
-func (ctx *context) parseAddressingMode() (int, *exprValue) {
+func (ctx *context) parseAddressingMode() (int, *exprValue, error) {
 	tok := ctx.lexer.getToken()
-	switch tok.( type ) {
+	switch tok.(type) {
 	case *tokNewLine:
-		return implicit, nil
+		return implicit, nil, nil
 	case *tokRegisterA:
-		return accumulator, nil
+		return accumulator, nil, nil
 	case *tokHash:
 		// Immediate addressing.
-		return immediate, ctx.expr()
+		return immediate, ctx.expr(), nil
 	case *tokLeftParen:
 		// Some form of indirect addressing.
 		return ctx.parseIndirect()
--- asm/addrmode_test.go
+++ asm/addrmode_test.go
@@ -3,39 +3,37 @@ package asm
 import "testing"
 
 func TestAdrMode(t *testing.T) {
-	var f42  int64 = 42
-	var f43  int64 = 43
+	var f42 int64 = 42
+	var f43 int64 = 43
 	for _, tc := range []struct {
-		str          string
+		str       string
 		wantError bool
-		wantMode   int
-		wantValue    *int64
+		wantMode  int
+		wantValue *int64
 	}{
 		{"", false, implicit, nil},
-		{"A",false, accumulator, nil},
-		{"#42",false, immediate, &f42},
-		{"#42,",true, immediate, &f42},
-		{"43",false, absolute, &f43},
-		{"43a",true, errorAddrMode, nil},
-		{"43,X",false, absoluteX, &f43},
-		{"43,Y",false, absoluteY, &f43},
-		{"43,Z",true, errorAddrMode, nil},
-		{"(42)",false, indirect, &f42},
-		{"(42):z",true, errorAddrMode, nil},
-		{"(42, X)",false, indexedIndirect, &f42},
-		{"(42),Y",false, indirectIndexed, &f42},
-		{"(42),Z",true, errorAddrMode, nil},
+		{"A", false, accumulator, nil},
+		{"#42", false, immediate, &f42},
+		{"43", false, absolute, &f43},
+		{"43,X", false, absoluteX, &f43},
+		{"43,Y", false, absoluteY, &f43},
+		{"43,Z", true, -1, nil},
+		{"(42)", false, indirect, &f42},
+		{"(42, X)", false, indexedIndirect, &f42},
+		{"(42, Y)", true, -1, nil},
+		{"(42),Y", false, indirectIndexed, &f42},
+		{"(42),Z", true, -1, nil},
 	} {
 		println(tc.str)
 		ctx := &context{
 			lexer: &lexer{newSourceFromString(tc.str), nil},
-			seg: newSegment(),
+			seg:   newSegment(),
 		}
 		ctx.seg.symbols["foo"] = &externSymbol{"foo"}
 		ctx.seg.symbols["bar"] = &localSymbol{"bar", 42, false}
 		ctx.seg.symbols["baz"] = &localSymbol{"bar", 1000, false}
-		mode, val := ctx.parseAddressingMode()
-		if ctx.errors != 0 && ! tc.wantError {
+		mode, val, err := ctx.parseAddressingMode()
+		if (err != nil && !tc.wantError) || (err == nil && tc.wantError) {
 			t.Errorf("parseAddressingMode() errors; got:%d, want:0", ctx.errors)
 		}
 		if mode != tc.wantMode {
@@ -54,4 +52,3 @@ func TestAdrMode(t *testing.T) {
 		}
 	}
 }
-
